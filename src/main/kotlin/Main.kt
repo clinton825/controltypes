31bd25e9d6244d7cacdc64c6a9c2365ff6badd3e
clintonbempah <20097793@mail.wit.ie>
2023-01-31 09:58:29 +0000

    add String templtes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+
 import java.lang.Integer.parseInt
 
 fun main(args: Array<String>) {
@@ -12,6 +13,18 @@ fun main(args: Array<String>) {
     println(describe(1000L))
     println(describe(2))
     println(describe("other"))
+
+    // for loop
+     forEachWithIndex()
+     forEachStandard()
+    forEachIndices()
+    whileWithList()
+    doWhileExample()
+    workingWithStrings()
+    workingWithTemplates()
+
+
+
 }
 
 fun maxOf(a: Int, b: Int) = if (a > b) {
@@ -78,12 +91,86 @@ fun printAnimal(aString: String) = when {
 }
 fun describe(obj: Any): String {
 
-    var result : String = when (obj){
+    var result: String = when (obj) {
         1 -> "One"
         "Hello" -> "A Greeting"
         is Long -> " Long Var"
         !is String -> "Not a String"
         else -> "Unknow"
     }
-    return  result
+    return result
 }
+
+fun forEachStandard(){
+var items = listOf("apple","banana","kiwi")
+    for (item in items){
+        println("Item is :$item")
+    }
+}
+
+fun forEachIndices(){
+    val items = listOf("apple","banana","kiwi")
+    for (index in items.indices){
+        println("The item at "+ index + "is:"+ items[index])
+    }
+
+}
+fun forEachWithIndex(){
+    val items = listOf("apple","banana","kiwi")
+    for ((index,value ) in items.withIndex()){
+        println("The item at"+ index + "is:" + value)
+    }
+
+}
+fun whileWithList() {
+    val items = listOf("apple", "banana","kiwi")
+    var index = 0
+    while (index < items .size){
+        println("Item at index:" + index + " is:" + items[index])
+        index++
+    }
+
+}
+fun doWhileExample(){
+    var index = 0
+    do {
+        println("Do While Index is: " + index )
+        index++
+    } while (index < 5)
+}
+fun workingWithStrings(){
+    val aString = "An escaped string!\n"
+    print(aString)
+
+    val einstein = """
+        Learn from yesterday
+        Live for today
+        Hope for tomorrow
+        The important thing
+        is not to stop questioning.
+        (Albert Einstein)
+    """.trimIndent()
+    println(einstein)
+
+    val franklin = """
+          |Tell me and I forget.
+          |Teach me and I remember.
+          |Involve me and I learn.
+          |  (Benjamin Franklin)
+    """.trimMargin()
+    println(franklin)
+}
+fun workingWithTemplates(){
+    var firstString = "This is a String"
+    var secondString = "and this is another String"
+
+    //using simple names
+    println ("Simple names: $firstString $secondString")
+
+    //using arbitrary expressions in {}
+    println("Expressions (cases): ${firstString.uppercase()} ${secondString.lowercase()}")
+    println("Expressions (string size): first string is ${firstString.length}")
+    println("Expressions (substring): ${firstString.substring(0,2)}")
+}
+
+
